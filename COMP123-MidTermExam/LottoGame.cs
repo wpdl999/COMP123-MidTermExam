f6537c339e9b6014bae27d099e0d604be0f8f813
Wooram Moon <wpdl555@naver.com>
2020-07-15 16:43:52 -0400

    added midterm

---
+++ COMP123-MidTermExam/LottoGame.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_MidTermExam
+{
+    /**
+     * <summary>
+     * This abstract class is a blueprint for all Lotto Games
+     * </summary>
+     * 
+     * @class LottoGame
+     * @property {int} ElementNum;
+     * @property {int} SetSize;
+     */
+    public abstract class LottoGame
+    {
+        // PRIVATE INSTANCE VARIABLES +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        // CREATE private fields here --------------------------------------------
+        List<int> _elememntList;
+        int _elementList;
+        List<int>_elementNumber;
+        int _numberList;
+        Random _random;
+        int _setSize;
+        // PUBLIC PROPERTIES ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        // CREATE public properties here -----------------------------------------
+        List<int> ElementList { get; set; }
+        int ElementNumber { get; set; }
+        List<int> NumberList { get; }
+        Random random { get; }
+        int SetSize { get; set; }
+        // CONSTRUCTORS +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        /**
+         * <summary>
+         * This constructor takes two parameters: elementNumber and setSize
+         * The elementNumber parameter has a default value of 6
+         * The setSize parameter has a default value of 49
+         * </summary>
+         * 
+         * @constructor LottoGame
+         * @param {int} elementNumber
+         * @param {int} setSize
+         */
+        public LottoGame(int elementNumber = 6, int setSize = 49)
+        {
+            // assign elementNumber local variable to the ElementNumber property
+            this.ElementNumber = elementNumber;
+
+            // assign setSize local variable tot he SetSize property
+            this.SetSize = setSize;
+
+            // call the _initialize method
+            this._initialize();
+
+            // call the _build method
+            this._build();
+        }
+
+        // PRIVATE METHODS ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        private LottoGame()
+        {
+        }
+        // CREATE the private _initialize method here -----------------------------
+        private void _initialize()
+        {
+            _elememntList = new Int32;
+            _elementNumber = new LottoGame();
+            _random = new Vector2D();
+        }
+        // CREATE the private _build method here -----------------------------------
+        private void _build()
+        { 
+            
+        }
+        // OVERRIDEN METHODS ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        /**
+         * <summary>
+         * Override the default ToString function so that it displays the current
+         * ElementList
+         * </summary>
+         * 
+         * @override
+         * @method ToString
+         * @returns {string}
+         */
+
+        public override string ToString()
+        {
+            // create a string variable named lottoNumberString and intialize it with the empty string
+            string lottoNumberString = String.Empty;
+
+            // for each lottoNumber in ElementList, loop...
+            foreach (int lottoNumber in ElementList)
+            {
+                // add lottoNumber and appropriate spaces to the lottoNumberString variable
+                lottoNumberString += lottoNumber > 9 ? (lottoNumber + " ") : (lottoNumber + "  ");
+            }
+
+            return lottoNumberString;
+        }
+
+        // PUBLIC METHODS +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        // CREATE the public PickElements method here ----------------------------
+        public void PickElements()
+        {
+
+        }
+    }
+}
